c570ddc49db0c1bc430d66f37d2298fa090b53cb
Jay Gudsson <gudsson@gmail.com>
2020-11-17 21:24:16 -0800

    folder fix

--- lesson3/practice_problems/easy2/q4.js
+++
@@ -1,8 +0,0 @@
-// Question 4
-
-// Starting with the string:
-let famousWords = "seven years ago...";
-// show two different ways to put the expected "Four score and " in front of it.
-
-console.log('Four score and ' + famousWords);
-console.log(`Four score and ${famousWords}`);
--- lesson3/practice_problems/medium1/q5.js
+++
@@ -1,5 +0,0 @@
-// Question 5
-
-// What will the following two lines of code output?
-console.log(0.3 + 0.6); //0.9
-console.log(0.3 + 0.6 === 0.9); //false
